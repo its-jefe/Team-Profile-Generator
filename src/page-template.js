91fcbe823b97be1fbde61ad5cd86f7c6c8bd419c
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-07-07 17:12:39 -0400

    changed style.css to dist folder and deleted the DS_Store that slipped through

--- src/page-template.js
+++ src/page-template.js
@@ -52,7 +52,7 @@ module.exports = function (passedObjectArray) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile Generator</title>
-        <link rel="stylesheet" href="../src/style.css">
+        <link rel="stylesheet" href="./style.css">
     </head>
     
     <body>
